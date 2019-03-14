4d0b6cc0d555dcbe5210d617cf3c317aef97efb3
sec9590 <sec9590@gmail.com>
2019-03-14 10:22:07 +0900

    first commit

---
+++ Ch01/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java World");
+		System.out.println("안녕하세요. 자바 세상");
+	}
+
+}
