7861aceba73d2640d56e3a106ee0f3635c5aa3ce
sec9590 <sec9590@gmail.com>
2019-04-12 11:17:26 +0900

    openchallenge modify

--- OpenChallenge/src/practice07_190411/ex02/HexaDemo.java
+++ OpenChallenge/src/practice07_190411/ex02/HexaDemo.java
@@ -1,29 +1,43 @@
 package practice07_190411.ex02;
 
 import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class HexaDemo {
 
 	public static void main(String[] args) {
 		try {
-			FileInputStream fis = new FileInputStream(
-					"D:\\workspace\\EGov\\Java\\Ch18\\bin\\section04\\ex02_file_input\\FileInputStreamExample.class");
-	
-			int data;
-			int count = 0;
-			
-			while ((data = fis.read()) != -1) {
-				count++;
-				String hexa = Integer.toHexString(data);
-				StringBuffer hexa_ = new StringBuffer(hexa);
-				if (hexa.length() == 1)
-					hexa_.insert(0, "0");
-				System.out.print(hexa_ + " ");
+			Scanner scan = new Scanner(System.in);
+			System.out.printf("찾을 파일명 > ");
+			String filePath = scan.nextLine();
 
-				if (count == 16) {
-					System.out.println();
-					count = 0;
+			FileInputStream fis = new FileInputStream(filePath);
+			byte[] buf = new byte[16];
+			int data;
+
+			int count = 0;
+
+			while ((data = fis.read(buf)) != -1) {
+				System.out.printf("%08X:  ", count);
+
+				for (int i = 0; i < data; i++) {
+					if (i == 8)
+						System.out.printf(" ");
+					System.out.printf("%02X ", buf[i]);
 				}
+
+				for (int i = 0; i <= (16 - data); i++)
+					System.out.printf("   ");
+
+				for (int i = 0; i < data; i++) {
+					if (buf[i] >= 32 && buf[i] <= 126)
+						System.out.printf("%c", buf[i]);
+					else
+						System.out.printf(".");
+				}
+
+				count += 16;
+				System.out.println();
 			}
 			fis.close();
 		} catch (Exception e) {
